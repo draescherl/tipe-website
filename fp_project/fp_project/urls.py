51cc0264de394760e2abbb570f0f95522087805d
Lucas Draescher <draescherl@eisti.eu>
2020-01-29 14:41:22 +0100

    Added login form

--- fp_project/fp_project/urls.py
+++ fp_project/fp_project/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls import include, url
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 if settings.DEBUG:
