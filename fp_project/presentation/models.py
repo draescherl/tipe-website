3e357638dc93fee9070aa4401cd4aab6032f1ec5
Lucas Draescher <draescherl@eisti.eu>
2020-02-28 21:44:08 +0100

    Added description on admin page

--- fp_project/presentation/models.py
+++ fp_project/presentation/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Slide(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    title = models.CharField(max_length=200)
-    content = models.CharField(max_length=2000)
-    notes = models.CharField(max_length=2000, default="None")
-    display = models.BooleanField(default=True)
+    name = models.CharField(max_length=200, unique=True, help_text="The id the div will be given.")
+    title = models.CharField(max_length=200, help_text="The title your slide will be given.")
+    content = models.CharField(max_length=2000, help_text="The general content of your slide.")
+    notes = models.CharField(max_length=2000, default="None", help_text="The hiddens notes for the slide.")
+    display = models.BooleanField(default=True, help_text="If unchecked the slide will not be displayed.")
     def __str__(self):
         return self.name
     class Meta:
