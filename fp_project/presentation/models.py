1155478c8c18669b868a22e7b7e86d235b0d5f2c
Lucas Draescher <draescherl@eisti.eu>
2020-02-28 19:46:14 +0100

    Added notes

--- fp_project/presentation/models.py
+++ fp_project/presentation/models.py
@@ -6,6 +6,7 @@ class Slide(models.Model):
     name = models.CharField(max_length=200, unique=True)
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=2000)
+    notes = models.CharField(max_length=2000, default="None")
     display = models.BooleanField(default=True)
     def __str__(self):
         return self.name
