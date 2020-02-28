c965441e9db2b368f4a5dd72966982beb14f098f
Lucas Draescher <draescherl@eisti.eu>
2020-02-28 19:39:19 +0100

    Fixed CSS

--- fp_project/fp_project/settings.py
+++ fp_project/fp_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e+nn8a#=cp^x1fg2jb4hi=ny-65z=03@bo(2=g_*+9vsuz2^7n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
